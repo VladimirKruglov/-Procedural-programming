23b4da3255eda51523e4620e57ddc3264d09b431
Kruglov Vladimir <80893929+VladimirKruglov@users.noreply.github.com>
2021-04-08 20:36:52 +0300

    Добавлена документация для всех методов

--- Task_1-1/main.cpp
+++ Task_1-1/main.cpp
@@ -3,14 +3,28 @@
 #include <cmath>
 using namespace std;
 
+/**
+* \brief Метод, считающий const a
+* \param const x, y, z
+* \return значение const a
+**/
 double GetA(const double x, const double y, const double z) {
 	return cbrt((x * y * z) + abs(z * sin(y))) ;
 }
+
+/**
+* \brief Метод, считающий const b
+* \param const x, y, z
+* \return значение const b
+**/
 double GetB(const double x, const double y, const double z) {
 	return y * cos(x * z * sin(y)) + 3;
 }
 
-
+/**
+ * \brief Точка входа в программу.
+ * \return Код ошибки (0 - успех).
+ */
 int main() {
 	const double x = 2.0;
 	const double y = 0.7;
