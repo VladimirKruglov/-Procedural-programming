30662854e45d6df3b67800d2c2e38ecd6400895e
Kruglov Vladimir <80893929+VladimirKruglov@users.noreply.github.com>
2021-04-14 15:39:41 +0300

    Изменения:
    1) Изменил документацию
    2) Объявление функции теперь предшествует реализации

--- Task_1-2/main.cpp
+++ Task_1-2/main.cpp
@@ -6,46 +6,35 @@ using namespace std;
 * \param byte - число байт
 * \return количество мегабайт в числе байт
 **/
-double Getm(double byte){
-	const int a = pow(10, 6);
-	return byte / a;
-}
+double Getm(double byte);
 
 /**
 * \brief Метод, переводящий байты в гигабайты
 * \param byte - число байт
 * \return количество гигабайт в числе байт
 **/
-double Getg(double byte){
-	const int b = pow(10, 9);
-	return byte / b;
-}
+double Getg(double byte);
 
-/**
-* \brief Метод, позволяющий ввести количество байт и проверяющий правильность ввода
-* \return правильное значение ввода количества байт
-**/
-double input() {
-	double byte;
-	cout << "Please enter number of bytes: ";
-	cin >> byte;
-	while (!cin || cin.get() != '\n') {
-		cout << "Wrong input! Please enter your number again: ";
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cin >> byte;
-	}
-	return byte;
-}
-	
 /**
  * \brief Точка входа в программу.
  * \return Код ошибки (0 - успех).
 **/
 int main(){
-	double byte = input();
+	double byte;
+	cout << "Please enter number of bytes: ";
+	cin >> byte;
 	double mega = Getm(byte), giga = Getg(byte);
 	cout << byte << " byte = " << mega << " megabyte\n" 
 		 << byte << " byte = " << giga << " gigabyte";
 	return 0;
+}
+
+double Getm(double byte) {
+	const int a = pow(10, 6);
+	return byte / a;
+}
+
+double Getg(double byte) {
+	const int b = pow(10, 9);
+	return byte / b;
 }
