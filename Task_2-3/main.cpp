df709fdff049965ce3bc55ae6730c5f524f46d4c
Kruglov Vladimir <80893929+VladimirKruglov@users.noreply.github.com>
2021-04-22 16:24:44 +0300

    Изменения в программе

--- Task_2-3/main.cpp
+++ Task_2-3/main.cpp
@@ -1,96 +1,63 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
-/**
-* \brief Ввод значения часов и проверка на правильность ввода
-* \return значение часа
-**/
-int input_hour();
+class Time {
+public:
+	int hour;
+	int minutes;
+};
 
 /**
-* \brief Ввод значения минут и проверка на правильность ввода
-* \return значение минут
+* \brief Метод, выводящий время
+* \param time - время
 **/
-int input_minutes();
-
-/**
-* \brief Метод, который добавляет ноль перед однозначными значениями часов и минут
-* \param hour - часы
-* \param minutes - минуты
-**/
-void timesDay(int hour, int minutes);
+void Print(Time time);
 
 /**
 * \brief Метод, определяющий время суток
-* \param hour - часы
+* \param time - время 
 **/
-void checkZero(int hour);
+void TimesDay(Time time);
 
 /**
 * \brief Точка входа в программу
 * \return Код ошибки (0-успех)
 **/
 int main() {
-	int hour, minutes;
-	hour = input_hour();
-	minutes = input_minutes();
-	timesDay(hour, minutes);
-	checkZero(hour);
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	Time time;
+	cout << "Введите значение часа: ";
+	cin >> time.hour;
+	cout << "Введите значение минут: ";
+	cin >> time.minutes;
+	Print(time);
+	TimesDay(time);
 	return 0;
 }
 
-int input_hour() { 
-	int hour;
-	cout << "Please enter hour: ";
-	cin >> hour;
-	while (hour < 0 || hour > 23 || !cin || (cin.get() != '\n'))
-	{
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cout << "Error! The entered value is incorrect or does not correspond to the frames of the day (00:00-23:59)\nRe-enter the value: ";
-		cin >> hour;
-	}
-	return hour;
+void Print(Time time) {
+	if (time.hour < 10 && time.minutes < 10)
+		cout << "Time 0" << time.hour << ":0" << time.minutes << "\n";
+	if (time.hour < 10 && time.minutes > 10)
+		cout << "Time 0" << time.hour << ":" << time.minutes << "\n";
+	if (time.hour > 10 && time.minutes < 10)
+		cout << "Time " << time.hour << ":0" << time.minutes << "\n";
+	if (time.hour > 10 && time.minutes > 10)
+		cout << "Time " << time.hour << ":" << time.minutes << "\n";
 }
 
-int input_minutes() {
-	int minutes;
-	cout << "Please enter minutes: ";
-	cin >> minutes;
-	while (minutes < 0 || minutes > 59 || !cin || (cin.get() != '\n'))
-	{
-		cin.clear();
-		while (cin.get() != '\n');
-		cout << "Error! Incorrect value or your number does not belong to [0; 59]. Re-enter the value: ";
-		cin >> minutes;
-	}
-	return minutes;
-}
-
-void timesDay(int hour, int minutes) {
-	if (hour < 10 && minutes < 10)
-		cout << "Time 0" << hour << ":0" << minutes << "\n";
-
-	if (hour < 10 && minutes > 10)
-		cout << "Time 0" << hour << ":" << minutes << "\n";
-
-	if (hour > 10 && minutes < 10)
-		cout << "Time " << hour << ":0" << minutes << "\n";
-
-	if (hour > 10 && minutes > 10)
-		cout << "Time " << hour << ":" << minutes << "\n";
-}
-
-void checkZero(int hour) {
-	if (hour >= 5 && hour <= 11)
+void TimesDay(Time time) {
+	if (time.hour >= 5 && time.hour <= 11)
 		cout << "Good morning!";
-	
-	if (hour > 11 && hour <= 16)
+
+	if (time.hour > 11 && time.hour <= 16)
 		cout << "Good day!";
-	
-	if (hour > 16 && hour <= 23)
+
+	if (time.hour > 16 && time.hour <= 23)
 		cout << "Good evening!";
-	
-	if (hour >= 0 && hour < 5)
+
+	if (time.hour >= 0 && time.hour < 5)
 		cout << "Good night!";
 }
