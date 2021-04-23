438624466f9562dd4b0d24479f2ff7a520388c8b
Kruglov Vladimir <80893929+VladimirKruglov@users.noreply.github.com>
2021-04-23 14:24:51 +0300

    Исправлены недочеты

--- Task_2-2/main.cpp
+++ Task_2-2/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 /**
-* \brief Функция, высчитывающая значение y, если x > 1
+* \brief Функция, расчитывающая значение y, если x > 1
 * \param a - константа, имеющая тип double
 * \param x - переменная, имеющая тип double
 * \return значение y
@@ -10,7 +11,7 @@ using namespace std;
 double GetY1(const double a, double x);
 
 /**
-* \brief Функция, высчитывающая значение y, если x <= 1
+* \brief Функция, расчитывающая значение y, если x <= 1
 * \param a - константа, имеющая тип double
 * \param x - переменная, имеющая тип double
 * \return значение y
@@ -18,28 +19,25 @@ double GetY1(const double a, double x);
 double GetY2(const double a, double x);
 
 /**
-* \brief Функция, высчитывающая значение y, если x <= 1
-* \param a - константа, имеющая тип double
-* \param double x - переменная, имеющая тип double
-* \return значение y
-**/
-double input();
-
-/**
 * \brief Точка входа в программу
 * \return Код ошибки (0-успех)
 **/
 int main() {
+	
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	const double a = 0.9;
-	double x = input();
+	double x;
+	cout << "Введите значение x: ";
+	cin >> x;
 	double y;
 	if (x > 1) {
 		y = GetY1(a, x);
-		cout << "Since x > 1, then y = " << y;
+		cout << "Так как x > 1, то y = " << y;
 	}
 	else  {
 		y = GetY2(a, x);
-		cout << "Since x <= 1, then y =  " << y;
+		cout << "Так как x <= 1, то y =  " << y;
 	}
 	return 0;
 }
@@ -52,16 +50,3 @@ double GetY2(const double a, double x) {
 	return 2 * a * cos(x) + 3 * x * x;
 }
 
-double input() {
-	double x;
-	cout << "Please enter x: ";
-	cin >> x;
-	while (!cin || (cin.get() != '\n'))
-	{
-		cout << "Error! Please enter x: ";
-		cin.clear();
-		cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		cin >> x;
-	}
-	return x;
-}
